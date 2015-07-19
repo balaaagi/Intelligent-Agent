fd18f08cb13bfd43bb92fd3f3249430506763729
Balaji S <balaaagi@gmail.com>
2015-07-19 18:30:44 +0530

    Added Cyclic Algo for D02

--- ravensproject/Agent.java
+++ ravensproject/Agent.java
@@ -11,6 +11,7 @@ import java.util.Set;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
+import java.text.DecimalFormat;
 /**
  * Your Agent for solving Raven's Progressive Matrices. You MUST modify this
  * file.
@@ -69,7 +70,7 @@ public class Agent {
             HashMap<String, RavensFigure> problemFigures=problem.getFigures();
             HashMap<String,Integer> problemInPixels=findthePixels(problemFigures);
             HashMap<String,String> problemTransformation=findtheTransformation(problemInPixels);
-            //answer=Integer.parseInt(findtheAnswer(problemInPixels,problemTransformation));
+            answer=Integer.parseInt(findtheAnswer(problemInPixels,problemTransformation,problemFigures));
             // Set<String> transformations=problemTransformation.keySet();
             // for(String s: transformations){
             //   System.out.println(s+"  "+problemTransformation.get(s));
@@ -83,6 +84,7 @@ public class Agent {
         }catch(Exception e){
           e.printStackTrace();
         }
+        System.out.println("Answer Option: "+answer);
         return answer;
     }
 
@@ -112,41 +114,28 @@ public class Agent {
  public HashMap<String,String> findtheTransformation(HashMap<String,Integer> problemInPixels){
    HashMap<String,String> transformation=new HashMap<String,String>();
    Set<String> optionNames=problemInPixels.keySet();
-  //  String firstRowComparison=
+
   String abTransition=findTransistion(problemInPixels.get("A"),problemInPixels.get("B"));
   String bcTransition=findTransistion(problemInPixels.get("B"),problemInPixels.get("C"));
   String deTransition=findTransistion(problemInPixels.get("D"),problemInPixels.get("E"));
   String efTransition=findTransistion(problemInPixels.get("E"),problemInPixels.get("F"));
   String ghTransition=findTransistion(problemInPixels.get("G"),problemInPixels.get("H"));
 
-  // System.out.println(findTransistion(problemInPixels.get("A"),problemInPixels.get("B")));
-  //  if(firstRowComparison.equalsIgnoreCase("no-change"))
-  if((abTransition==bcTransition)&&(deTransition==efTransition))
+
+  if((abTransition==bcTransition)&&(deTransition==efTransition)){
       transformation.put("pixelChange","no-change");
       transformation.put("transitionChange","no-change");
+  }
  else{
    if((abTransition==deTransition)&&(bcTransition==efTransition)){
      transformation.put("pixelChange","column");
      transformation.put("transitionChange","column");
    }else{
-     tranformation.put("transitionChange","cyclic");
+     transformation.put("transitionChange","cyclic");
      transformation.put("pixelChange","cyclic");
    }
 
  }
-
-
-  System.out.println(findTransistion(problemInPixels.get("B"),problemInPixels.get("C")));
-  System.out.println(findTransistion(problemInPixels.get("D") ,problemInPixels.get("E")));
-  System.out.println(findTransistion(problemInPixels.get("E"),problemInPixels.get("F")));
-  System.out.println(findTransistion(problemInPixels.get("G"),problemInPixels.get("H")));
-    //else
-    //  transformation.put("pixelChange","")
-    // transformation.put("1",);
-    // transformation.put("2",);
-    // transformation.put("3",findTransistion(problemInPixels.get("D"),problemInPixels.get("E")));
-    // transformation.put("4",findTransistion(problemInPixels.get("E"),problemInPixels.get("F")));
-    //    //String firstColumnComparison=findTransistion(problemInPixels.get("A"),problemInPixels.get("D"),problemInPixels.get("G"));
 
    return transformation;
  }
@@ -165,12 +154,124 @@ if((a==b))
     return transition;
  }
 
- public String findtheAnswer(HashMap<String,Integer> problemInPixels,HashMap<String,String> tranformation){
+ public String findtheAnswer(HashMap<String,Integer> problemInPixels,HashMap<String,String> transformation,HashMap<String,RavensFigure> problemFigures ){
    String theAnswer="";
-   if(tranformation.get("pixelChange").equalsIgnoreCase("no-change")){
-              theAnswer=compareAnswer(problemInPixels.get("G"),problemInPixels);
+   if(transformation.get("pixelChange").equalsIgnoreCase("no-change")){
+              theAnswer=findAnswerConstant(problemInPixels.get("G"),problemInPixels);
+   }else if(transformation.get("transitionChange").equalsIgnoreCase("column")){
+            System.out.println("This Problem is Column Wise");
+              //theAnswer=findAnswerColumn(problemInPixels.get("G"));
+   }else{
+          System.out.println("This Problem is Cyclic ");
+          theAnswer=findAnswerCyclic(problemInPixels);
    }
    return theAnswer;
+ }
+
+ public String findAnswerConstant(int a , HashMap<String,Integer> problemInPixels){
+  String answer="";
+  String numRegex="^\\d+$";
+  HashMap<String,Double> answers=new HashMap<String,Double>();
+  Set<String> problem=problemInPixels.keySet();
+  for(String option:problem){
+    if(option.matches(numRegex)){
+      double accuracyPercentage=(Math.abs(problemInPixels.get(option)-a)/(double)((a>problemInPixels.get(option))?problemInPixels.get(option):a));
+      if(problemInPixels.get(option)==a)
+        answer=option;
+      else{
+        if(accuracyPercentage<0.5)
+          answers.put(option,accuracyPercentage);
+      }
+
+    }
+  }
+  if(answer.equalsIgnoreCase("")){
+      Set<String> accuracyOptions=answers.keySet();
+      double nearestAccuracy=0.5;
+
+      for(String s: accuracyOptions){
+        if(answers.get(s)<nearestAccuracy){
+            nearestAccuracy=answers.get(s);
+            answer=s;
+        }
+      }
+
+
+  }
+return answer;
+ }
+
+ public String findAnswerCyclic(HashMap<String,Integer> problemInPixels){
+   String theAnswer="";
+   String numRegex="^\\d+$";
+   Set<String> figureNames=problemInPixels.keySet();
+   HashMap<String,Double> answers=new HashMap<String,Double>();
+   HashMap<Integer,Integer> pixelsCyclic=new HashMap<Integer,Integer>();
+   for(String figureName:figureNames){
+     Integer optionPixels=problemInPixels.get(figureName);
+
+      if(figureName.matches(numRegex)){
+
+      }else{
+
+        if(pixelsCyclic.containsKey(optionPixels)){
+
+           int newValue=pixelsCyclic.get(optionPixels);
+           newValue=newValue+1;
+
+            pixelsCyclic.put(optionPixels,newValue);
+        }else{
+
+          pixelsCyclic.put(optionPixels,1);
+        }
+      }
+   }
+  Set<Integer> pixelsCyclicValues=pixelsCyclic.keySet();
+  int expectedAnswerPixels=-1;
+  for(Integer a: pixelsCyclicValues){
+
+    if(pixelsCyclic.get(a)<3){
+      expectedAnswerPixels=a;
+      break;
+    }
+  }
+
+  if(expectedAnswerPixels!=-1){
+    for (String optionName : figureNames) {
+
+      if(optionName.matches(numRegex)){
+
+
+        double accuracyPercentage=(Math.abs(problemInPixels.get(optionName)-expectedAnswerPixels)/
+        (double)((expectedAnswerPixels>problemInPixels.get(optionName))?problemInPixels.get(optionName):expectedAnswerPixels));
+
+
+
+        if(problemInPixels.get(optionName)==expectedAnswerPixels){
+          theAnswer=optionName;
+          break;
+        }else{
+          if(accuracyPercentage<0.5)
+            answers.put(optionName,accuracyPercentage);
+
+        }
+      }
+      if(theAnswer.equalsIgnoreCase("")){
+          Set<String> accuracyOptions=answers.keySet();
+          double nearestAccuracy=0.5;
+
+          for(String s: accuracyOptions){
+            if(answers.get(s)<nearestAccuracy){
+                nearestAccuracy=answers.get(s);
+                theAnswer=s;
+            }
+          }
+
+
+      }
+    }
+  }
+  return theAnswer;
  }
 
  public String compareAnswer(int a,HashMap<String,Integer> problemInPixels){
@@ -183,7 +284,7 @@ if((a==b))
       //  System.out.println(a+"=="+problemInPixels.get(option));
         if(problemInPixels.get(option)==a)
           answer=option;
-      double r=Math.abs(problemInPixels.get(option)-a)*100/((problemInPixels.get(option)>a)?problemInPixels.get(option):a);
+      double r=Math.abs(problemInPixels.get(option)-a)*100/((problemInPixels.get(option)>a)?a:problemInPixels.get(option));
       System.out.println(r);
         if(r>98)
             probans.put(option,r);
