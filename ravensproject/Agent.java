63f97b62d09aacfe6a049f426b75a68da5827459
Balaji S <balaaagi@gmail.com>
2015-07-20 16:11:30 +0530

    Latest Changes

--- ravensproject/Agent.java
+++ ravensproject/Agent.java
@@ -71,20 +71,21 @@ public class Agent {
             HashMap<String,Integer> problemInPixels=findthePixels(problemFigures);
             HashMap<String,String> problemTransformation=findtheTransformation(problemInPixels);
             HashMap<String,Integer[][]> problemInPixelMap=findPixelMap(problemFigures);
-            answer=Integer.parseInt(findtheAnswer(problemInPixels,problemTransformation,problemFigures,problemInPixelMap));
+           answer=Integer.parseInt(findtheAnswer(problemInPixels,problemTransformation,problemFigures,problemInPixelMap));
             
             // Set<String> problemInPixelMapoptions=problemInPixelMap.keySet();
             // for(String option:problemInPixelMapoptions){
               // Integer[][] thisoptionA=problemInPixelMap.get("7");
-              // for(int i=0;i<184;i++){
-              //   for(int j=0;j<184;j++){
-              //     if(thisoptionA[j][i]==0)
-              //       System.out.print(".");
-              //     else
-              //       System.out.print(" ");
-              //   }
-              //   System.out.println();
-              // }
+            // Integer[][] diffAD=xorTwoOptions(problemInPixelMap.get("A"),problemInPixelMap.get("B"));
+            //   for(int i=0;i<184;i++){
+            //     for(int j=0;j<184;j++){
+            //       if(diffAD[j][i]==0)
+            //         System.out.print(".");
+            //       else
+            //         System.out.print(" ");
+            //     }
+            //     System.out.println();
+            //   }
             // }
 
            System.out.println("Visual is avaialable");
@@ -132,15 +133,15 @@ public class Agent {
   String deTransition=findTransistion(problemInPixels.get("D"),problemInPixels.get("E"));
   String efTransition=findTransistion(problemInPixels.get("E"),problemInPixels.get("F"));
   String ghTransition=findTransistion(problemInPixels.get("G"),problemInPixels.get("H"));
-  // System.out.println(abTransition);
-  // System.out.println(bcTransition);
-  // System.out.println(deTransition);
-  // System.out.println(efTransition);
-  // System.out.println(ghTransition);
+  System.out.println(abTransition);
+  System.out.println(bcTransition);
+  System.out.println(deTransition);
+  System.out.println(efTransition);
+  System.out.println(ghTransition);
 
   if((abTransition==bcTransition)&&(deTransition==efTransition)){
-      transformation.put("pixelChange","no-change");
-      transformation.put("transitionChange","no-change");
+        transformation.put("pixelChange","no-change");
+        transformation.put("transitionChange","no-change");  
   }
  else{
    if((abTransition==deTransition)&&(bcTransition==efTransition)){
@@ -173,18 +174,34 @@ public class Agent {
 
  public String findtheAnswer(HashMap<String,Integer> problemInPixels,HashMap<String,String> transformation,HashMap<String,RavensFigure> problemFigures,HashMap<String,Integer[][]> problemPixelMap ){
    String theAnswer="";
+   int typeOfProblem=-1;
+   typeOfProblem=findProblemPattern(problemPixelMap);
+   System.out.println("Problem is of Type "+typeOfProblem);
    if(transformation.get("pixelChange").equalsIgnoreCase("no-change")){
-            System.out.println("This Problem is Costant");
+            System.out.println("This Problem is Constant");
+            // if(typeOfProblem==-1)
               theAnswer=findAnswerConstant(problemInPixels.get("G"),problemInPixels);
+            // else
+            //    theAnswer=findAnswerCyclicPattern(typeOfProblem,problemPixelMap,problemInPixels);
    }else if(transformation.get("transitionChange").equalsIgnoreCase("column")){
             System.out.println("This Problem is Column Wise");
-            theAnswer=findAnswerColumn(problemInPixels,problemFigures,problemPixelMap);
+            if(typeOfProblem==-1)
+              theAnswer=findAnswerColumn(problemInPixels,problemFigures,problemPixelMap);
+            else
+              theAnswer=findAnswerCyclicPattern(typeOfProblem,problemPixelMap,problemInPixels);
    }else{
           System.out.println("This Problem is Cyclic ");
-          theAnswer=findAnswerCyclic(problemInPixels);
+          
+          
+          if(typeOfProblem==-1)
+            theAnswer=findAnswerCyclic(problemInPixels,problemPixelMap);
+          else
+            theAnswer=findAnswerCyclicPattern(typeOfProblem,problemPixelMap,problemInPixels);
    }
    return theAnswer;
  }
+
+ 
 
  public String findAnswerConstant(int a , HashMap<String,Integer> problemInPixels){
   String answer="";
@@ -219,7 +236,127 @@ public class Agent {
 return answer;
  }
 
- public String findAnswerCyclic(HashMap<String,Integer> problemInPixels){
+ public String findAnswerCyclicPattern(int pattern,HashMap<String,Integer[][]> problemPixelMap,HashMap<String,Integer> problemInPixels){
+  String theAnswer="";
+  String numRegex="^\\d+$";
+  Set<String> optionNames=problemPixelMap.keySet();
+  Integer[][] probableAnswer=null;
+  double answerAccuracy=0.0;
+  if(pattern==1){
+    probableAnswer=mergeTwoOptions(problemPixelMap.get("G"),problemPixelMap.get("H"));
+  }else if(pattern==2){
+    probableAnswer=mergeTwoOptions(problemPixelMap.get("C"),problemPixelMap.get("F"));
+  }else if(pattern==3){
+    probableAnswer=diffTwoOptions(problemPixelMap.get("C"),problemPixelMap.get("F"));
+  }else if(pattern==4){
+    probableAnswer=diffTwoOptions(problemPixelMap.get("G"),problemPixelMap.get("H"));
+  }else if(pattern==5){
+    probableAnswer=diffTwoOptions(problemPixelMap.get("H"),problemPixelMap.get("G"));
+  }else if(pattern==6){
+    probableAnswer=interesctTwoOptions(problemPixelMap.get("G"),problemPixelMap.get("H"));
+  }else if(pattern==7){
+    probableAnswer=interesctTwoOptions(problemPixelMap.get("C"),problemPixelMap.get("F"));
+  }
+  
+  for(String optionName:optionNames){
+    if(optionName.matches(numRegex)){
+          double probableAnswerAccuracyWithOption=compareObjects(probableAnswer,problemPixelMap.get(optionName));
+          if(probableAnswerAccuracyWithOption>answerAccuracy){
+            answerAccuracy=probableAnswerAccuracyWithOption;
+            theAnswer=optionName;
+            System.out.println("Option "+optionName+" Accuracy"+probableAnswerAccuracyWithOption);
+          }
+          
+    
+    }
+      
+  }
+  
+  return theAnswer;
+ }
+
+ public int findProblemPattern(HashMap<String,Integer[][]> problemPixelMap){
+  int pattern=-1;
+  String numRegex="^\\d+$";
+  Set<String> optionNames=problemPixelMap.keySet();
+  Integer[][] mergeAB=mergeTwoOptions(problemPixelMap.get("A"),problemPixelMap.get("B"));
+  Integer[][] mergeDE=mergeTwoOptions(problemPixelMap.get("D"),problemPixelMap.get("E"));
+  double accuracyPercentageAB=compareObjects(mergeAB,problemPixelMap.get("C"));
+  double accuracyPercentageDE=compareObjects(mergeDE,problemPixelMap.get("F"));
+  if(((accuracyPercentageAB>0.99)&&(accuracyPercentageAB<=1.0))&&((accuracyPercentageDE>0.9)&&(accuracyPercentageDE<=1.0))){
+    pattern=1;
+    System.out.println("This Problem is Horizontal Sum with accuracy "+accuracyPercentageAB);
+  }else{
+    Integer[][] mergeAD=mergeTwoOptions(problemPixelMap.get("A"),problemPixelMap.get("D"));
+    Integer[][] mergeBE=mergeTwoOptions(problemPixelMap.get("B"),problemPixelMap.get("E"));
+    double accuracyPercentageAD=compareObjects(mergeAB,problemPixelMap.get("G"));
+    double accuracyPercentageBE=compareObjects(mergeBE,problemPixelMap.get("H"));
+
+    if(((accuracyPercentageAD>0.99)&&(accuracyPercentageAD<=1.0))&&((accuracyPercentageBE>0.99)&&(accuracyPercentageBE<=1.0))){
+      pattern=2;
+      System.out.println("This Problem is Vertical Sum with accuracy "+accuracyPercentageAB);
+    }
+    else{
+      Integer[][] diffAD=diffTwoOptions(problemPixelMap.get("A"),problemPixelMap.get("D"));
+      Integer[][] diffBE=diffTwoOptions(problemPixelMap.get("B"),problemPixelMap.get("E"));
+      double accuracyPercentageADDiff=compareObjects(diffAD,problemPixelMap.get("G"));
+      double accuracyPercentageBEDiff=compareObjects(diffBE,problemPixelMap.get("H"));
+
+      if(((accuracyPercentageADDiff>0.9)&&(accuracyPercentageADDiff<=1.0))&&((accuracyPercentageBEDiff>0.9)&&(accuracyPercentageBEDiff<=1.0))){
+        System.out.println("The problem is vertical Difference");
+        System.out.println("Accuracy for Difference "+accuracyPercentageBEDiff);
+        pattern=3;  
+      }else{
+        Integer[][] diffAB=diffTwoOptions(problemPixelMap.get("A"),problemPixelMap.get("B"));
+        Integer[][] diffDE=diffTwoOptions(problemPixelMap.get("D"),problemPixelMap.get("E"));
+        double accuracyPercentageABDiff=compareObjects(diffAB,problemPixelMap.get("C"));
+        double accuracyPercentageDEDiff=compareObjects(diffDE,problemPixelMap.get("F"));
+        if(((accuracyPercentageABDiff>0.9)&&(accuracyPercentageABDiff<=1.0))&&((accuracyPercentageDEDiff>0.9)&&(accuracyPercentageDEDiff<=1.0))){
+          System.out.println("The problem is Horizontal Difference");
+          pattern=4;
+        }else{
+        Integer[][] diffBA=diffTwoOptions(problemPixelMap.get("D"),problemPixelMap.get("A"));
+        Integer[][] diffED=diffTwoOptions(problemPixelMap.get("E"),problemPixelMap.get("B"));
+        double accuracyPercentageBADiff=compareObjects(diffBA,problemPixelMap.get("G"));
+        double accuracyPercentageEDDiff=compareObjects(diffED,problemPixelMap.get("H"));
+        if(((accuracyPercentageBADiff>0.9)&&(accuracyPercentageBADiff<=1.0))&&((accuracyPercentageEDDiff>0.9)&&(accuracyPercentageEDDiff<=1.0))){
+         System.out.println("The problem is Horizontal Difference Reverese");
+         pattern=5;
+        }else{
+          Integer[][] intersectAB=interesctTwoOptions(problemPixelMap.get("A"),problemPixelMap.get("B"));
+          Integer[][] intersectDE=interesctTwoOptions(problemPixelMap.get("D"),problemPixelMap.get("E"));
+          double accuracyPercentageABInter=compareObjects(intersectAB,problemPixelMap.get("C"));
+          double accuracyPercentageDEInter=compareObjects(intersectDE,problemPixelMap.get("F"));
+          if(((accuracyPercentageABInter>0.9)&&(accuracyPercentageABInter<=1.0))&&((accuracyPercentageDEInter>0.9)&&(accuracyPercentageDEInter<=1.0))){
+              System.out.println("The problem is Horizontal Intersection"); 
+              pattern=6; 
+          }else{
+            Integer[][] intersectAD=interesctTwoOptions(problemPixelMap.get("A"),problemPixelMap.get("D"));
+            Integer[][] intersectBE=interesctTwoOptions(problemPixelMap.get("B"),problemPixelMap.get("E"));
+            double accuracyPercentageADInter=compareObjects(intersectAD,problemPixelMap.get("G"));
+            double accuracyPercentageBEInter=compareObjects(intersectBE,problemPixelMap.get("H"));
+            if(((accuracyPercentageADInter>0.9)&&(accuracyPercentageADInter<=1.0))&&((accuracyPercentageBEInter>0.9)&&(accuracyPercentageBEInter<=1.0))){
+              System.out.println("The problem is Vertical Intersection"); 
+              pattern=7; 
+            }
+          }
+          
+        }
+       
+        }
+
+      }
+      
+    }
+  }
+    
+  
+
+  return pattern;
+
+ }
+
+ public String findAnswerCyclic(HashMap<String,Integer> problemInPixels,HashMap<String,Integer[][]> problemPixelMap){
    String theAnswer="";
    String numRegex="^\\d+$";
    Set<String> figureNames=problemInPixels.keySet();
@@ -229,25 +366,27 @@ return answer;
    int ePixels=problemInPixels.get("E");
    int cPixels=problemInPixels.get("C");
    int gPixels=problemInPixels.get("G");
-   for(String figureName:figureNames){
-     Integer optionPixels=problemInPixels.get(figureName);
 
-      if(figureName.matches(numRegex)){
+   
+     for(String figureName:figureNames){
+            Integer optionPixels=problemInPixels.get(figureName);
 
-      }else{
-       // System.out.println("Option "+ figureName+"Pixel "+optionPixels);
-        if(pixelsCyclic.containsKey(optionPixels)){
+            if(figureName.matches(numRegex)){
 
-           int newValue=pixelsCyclic.get(optionPixels);
-           newValue=newValue+1;
+            }else{
+             // System.out.println("Option "+ figureName+"Pixel "+optionPixels);
+              if(pixelsCyclic.containsKey(optionPixels)){
 
-            pixelsCyclic.put(optionPixels,newValue);
-        }else{
+                 int newValue=pixelsCyclic.get(optionPixels);
+                 newValue=newValue+1;
 
-          pixelsCyclic.put(optionPixels,1);
-        }
+                  pixelsCyclic.put(optionPixels,newValue);
+              }else{
+
+                pixelsCyclic.put(optionPixels,1);
+              }
+            }
       }
-   }
   Set<Integer> pixelsCyclicValues=pixelsCyclic.keySet();
   int expectedAnswerPixels=-1;
   for(Integer a: pixelsCyclicValues){
@@ -334,10 +473,11 @@ boolean diagonalPercentageTrack=false;
           }
         
 
-        
-    }
-  }
-}
+              
+          }
+        }
+      }
+
   return theAnswer;
  }
 
@@ -396,7 +536,7 @@ String numRegex="^\\d+$";
             Integer[][] optionCompare=findCommonObject(merged,problemPixelMap.get(optionName));
             double accuracyPercentage=compareObjects(merged,optionCompare);
             double accuracyPercentage2=(Math.abs(expectedPixelCount-optionPixels))/(double)((expectedPixelCount>optionPixels)?optionPixels:expectedPixelCount);
-            // System.out.println("Option "+optionName+" PixelCount "+optionPixels+" Accuracy1 "+accuracyPercentage+" Accuracy2 "+accuracyPercentage2);     
+            System.out.println("Option "+optionName+" PixelCount "+optionPixels+" Accuracy1 "+accuracyPercentage+" Accuracy2 "+accuracyPercentage2);     
             if(accuracyPercentage==nearestLowAccuracy){
               
                 nearestLowAccuracy=accuracyPercentage;
@@ -450,6 +590,36 @@ public Integer[][] mergeTwoOptions(Integer[][] a,Integer[][] b){
   for(int i=0;i<184;i++){
     for(int j=0;j<184;j++){
       colCommonObject[j][i]=a[j][i]|b[j][i];
+    }
+  }
+  return colCommonObject;
+}
+
+public Integer[][] diffTwoOptions(Integer[][] a,Integer[][] b){
+  Integer[][] colCommonObject=new Integer[184][184];
+  for(int i=0;i<184;i++){
+    for(int j=0;j<184;j++){
+      colCommonObject[j][i]=a[j][i]-b[j][i];
+    }
+  }
+  return colCommonObject;
+}
+
+public Integer[][] xorTwoOptions(Integer[][] a,Integer[][] b){
+  Integer[][] colCommonObject=new Integer[184][184];
+  for(int i=0;i<184;i++){
+    for(int j=0;j<184;j++){
+      colCommonObject[j][i]=a[j][i] ^ b[j][i];
+    }
+  }
+  return colCommonObject;
+}
+
+public Integer[][] interesctTwoOptions(Integer[][] a,Integer[][] b){
+  Integer[][] colCommonObject=new Integer[184][184];
+  for(int i=0;i<184;i++){
+    for(int j=0;j<184;j++){
+      colCommonObject[j][i]=a[j][i]&b[j][i];
     }
   }
   return colCommonObject;
